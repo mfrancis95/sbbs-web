152df75e1c557abcd4dac5ed56b9c4018b7c9b48
mfrancis95 <mikefrancis95@gmail.com>
2016-07-08 11:30:50 -0400

    Moved some things around. Changed all of the yellows into blues. Replace their page scrolling with https://github.com/mfrancis95/smooth-scroll because mine is easier to customise. Added an inset box-shadow to the header so the whites won't be lost in the background.

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ var app = express();
 app.use(require("compression")());
 app.use(require("morgan")("common"));
 app.use(express.static("public", {
-    maxAge: 1209600000
+    //maxAge: 1209600000 Uncomment this when going live
 }));
 
 app.use(function(request, response) {
-    response.redirect("/");
+    response.redirect("/sbbs/"); //Change this to "/" when we get rid of the templates
 });
 
 app.listen(80, function() {
