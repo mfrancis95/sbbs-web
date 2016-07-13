769c52a95e060186f7874fc97bc04be1742e1d9a
dkeefe95 <daniel.keefe@stonybrook.edu>
2016-07-13 14:39:58 -0400

    Made SBBS link to homepage on three service websites. Added side toolbar for navigation on controls page.  Made includes section for scripts, css, and footer.

--- server.js
+++ server.js
@@ -6,17 +6,29 @@ app.set("view engine", "ejs");
 app.use(require("compression")());
 app.use(require("morgan")("common"));
 
-app.use("/static", express.static("public", { //Rename the folder from public to static
+app.use("/static", express.static("static", { //Rename the folder from public to static
     //maxAge: 1209600000 Uncomment this when going live
 }));
 
 //Routes
 //Add a route for each view
 
-app.get("/", function(request, response) { //Change this to be the SBBS index
+app.get("/", function(request, response) {
     response.render("pages/index");
 });
 
-app.listen(80, function() {
+app.get("/energyModeling", function(request, response) {
+    response.render("pages/energyModeling");
+});
+
+app.get("/mechSystems", function(request, response) {
+    response.render("pages/mechSystems");
+});
+
+app.get("/controls", function(request, response) {
+    response.render("pages/controls");
+});
+
+app.listen(8081, function() {
     console.log("Started web server.");
 });
