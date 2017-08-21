8814650a5798f1218682f87953bd7273a0d8044a
mfrancis95 <mikefrancis95@gmail.com>
2017-08-21 13:27:09 -0400

    Make default port 80 and update node modules

--- server.js
+++ server.js
@@ -1,24 +1,23 @@
 var config = require("./" + process.argv[2] + ".json");
 var express = require("express");
-var viewServe = require("./view-serve");
 var app = express();
- 
+
 app.set("view engine", "ejs");
- 
+
 app.use(require("compression")());
- 
+
 if (config.minify) {
     app.use(require("express-minify")());
 }
- 
+
 if (config.logging) {
     app.use(require("morgan")("common"));
 }
- 
+
 app.use(express.static("static", {
     maxAge: config.maxAge ? config.maxAge : 0
 }));
- 
+
 if (config.construction) {
     app.get("/", function(request, response) {
         response.render("special/construction", {
@@ -27,15 +26,15 @@ if (config.construction) {
     });
 }
 else {
-    app.use(viewServe("pages", "./locals.json"));
+    app.use(require("view-serve")("pages", require("path").resolve(__dirname, "locals.json")));
 }
- 
+
 app.use(function(request, response) {
     response.render("special/404", {
         title: "Page Not Found"
     });
 });
- 
-app.listen(config.port, function() {
+
+app.listen(config.port || 80, function() {
     console.log("Started web server.");
 });
